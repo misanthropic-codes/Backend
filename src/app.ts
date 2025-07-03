048be4a450ef464724386a28c75b1eb437125de3
misanthropic-codes <airabhishek098@gmail.com>
2025-07-03 16:27:28 +0530

    updated routes

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,31 @@ import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth";
 import groupRoutes from "./routes/groups.route";
+import postRoutes from "./routes/posts.route";
 import { errorHandler } from "./middleware/error.middleware";
+import { helmetMiddleware } from './middlewares/helmet.middleware';
+import { compressionMiddleware } from './middlewares/compression.middleware';
+import { morganMiddleware } from './middlewares/morgan.middleware';
 
 // Load environment variables
 dotenv.config();
 
+
 const app = express();
+app.use(helmetMiddleware);
+app.use(compressionMiddleware);
+app.use(morganMiddleware);
 
 app.use(express.json());
 
 // Routes
 
+
 app.use('/auth', authRoutes);
+
+
 app.use('/groups', groupRoutes);
+app.use('/', postRoutes);
 
 
 // Health check
